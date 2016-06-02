abb9fe932886ac981ba7dbc752129efd3ec89c94
MrLokans <trikster1911@gmail.com>
2016-06-02 14:02:54 +0300

    Argparse is used to create basic CLI.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # coding: utf8
 
+import os
 import json
+import argparse
 
 from parser import KoshelekParser
 from exporters import CSVExporter
@@ -12,13 +14,38 @@ SETTINGS_FILE = "settings.json"
 SETTINGS = json.load(open(SETTINGS_FILE))
 
 
+def parse_args():
+    arg_parser = argparse.ArgumentParser(description='Parse costs and income data from koshelek.org.')
+    arg_parser.add_argument("--login",
+                            help="Login for www.koshelek.org account.",
+                            type=str)
+    arg_parser.add_argument("--password",
+                            help="Password for www.koshelek.org account.",
+                            type=str)
+    arg_parser.add_argument("--settings",
+                            help="Path to JSON file with password and login.")
+    args = arg_parser.parse_args()
+    return args
+
+
 def main():
-    parser = KoshelekParser(username=SETTINGS["login"],
-                            password=SETTINGS["password"],
-                            exporter=CSVExporter())
+    args = parse_args()
+    if args.login and args.password:
+        parser = KoshelekParser(username=args.login,
+                                password=args.password,
+                                exporter=CSVExporter())
+    elif args.settings:
+        assert os.path.exists(args.settings)
+        assert os.path.isfile(args.settings)
+
+        settings = json.load(open(args.settings))
+        parser = KoshelekParser(username=settings["login"],
+                                password=settings["password"],
+                                exporter=CSVExporter())
+    else:
+        raise ValueError("Either login/password should be specified or settings file.")
     all_costs = parser.get_costs_for_months(months=2)
-    parser.export_to_file(all_costs, "all_costs.csv", delimeter="|")
-    print(all_costs)
+    parser.export_to_file(all_costs, "all_costs.csv", delimeter=CSV_DELIMETER)
 
 if __name__ == '__main__':
     main()
