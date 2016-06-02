214fe97a7496c811fa4f1f59aaab04b611acc0d4
MrLokans <trikster1911@gmail.com>
2016-06-02 14:11:34 +0300

    Settings may now be stored in the JSON file.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import json
 import argparse
 
+from exceptions import SettingsValidationError
 from parser import KoshelekParser
 from exporters import CSVExporter
 
@@ -12,6 +13,21 @@ CSV_DELIMETER = '|'
 
 SETTINGS_FILE = "settings.json"
 SETTINGS = json.load(open(SETTINGS_FILE))
+
+
+def load_settings_from_file(filepath):
+    with open(filepath) as settings_fh:
+        return json.load(settings_fh)
+
+
+def validate_settings_dict(settings_dict):
+    """
+    Check whether settings dict has all the required fields
+    """
+    required_fields = ("login", "password")
+    if not all(x in settings_dict for x in required_fields):
+        msg = "Incorrect settings, following fields are required: {}."
+        raise SettingsValidationError(msg.format(", ".join(required_fields)))
 
 
 def parse_args():
@@ -38,12 +54,22 @@ def main():
         assert os.path.exists(args.settings)
         assert os.path.isfile(args.settings)
 
-        settings = json.load(open(args.settings))
+        settings = load_settings_from_file(args.settings)
+        validate_settings_dict(settings)
+
+        parser = KoshelekParser(username=settings["login"],
+                                password=settings["password"],
+                                exporter=CSVExporter())
+    elif os.path.exists(SETTINGS_FILE) and os.path.isfile(SETTINGS_FILE):
+        settings = load_settings_from_file(SETTINGS_FILE)
+        validate_settings_dict(settings)
+
         parser = KoshelekParser(username=settings["login"],
                                 password=settings["password"],
                                 exporter=CSVExporter())
     else:
-        raise ValueError("Either login/password should be specified or settings file.")
+        msg = "Either login/password should be specified or settings file."
+        raise ValueError(msg)
     all_costs = parser.get_costs_for_months(months=2)
     parser.export_to_file(all_costs, "all_costs.csv", delimeter=CSV_DELIMETER)
 
