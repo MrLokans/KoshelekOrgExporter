4af39c5c87e4682e5f0b5be47b746e192fe1ff96
MrLokans <MrLokans@gmail.com>
2017-09-30 13:55:38 +0300

    CSV exported now handles generic namedtuples.

--- exporters/csv_exporter.py
+++ exporters/csv_exporter.py
@@ -1,18 +1,25 @@
 import csv
+import itertools
+import typing
+from collections import namedtuple
 
 
 class CSVExporter(object):
 
     @staticmethod
-    def export_to_file(costs, filename, **kwargs):
+    def export_to_file(costs: typing.Sequence[namedtuple],
+                       filename: str,
+                       **kwargs):
         delimiter = kwargs.get('delimeter', ',')
         # Description and title may contain commas
         with open(filename, "w") as csv_f:
+            if not costs:
+                return
+            first = next(iter(costs))
             writer = csv.writer(csv_f, delimiter=delimiter)
-            writer.writerow(['Title', 'Description', 'Category', 'Budget',
-                             'Currency', 'Sum', 'Account', 'Date'])
+            writer.writerow(first._fields)
 
-            for cost in costs:
-                writer.writerow([cost.title, cost.description, cost.category,
-                                 cost.budget, cost.currency, cost.value,
-                                 cost.account, cost.date])
+            for cost in itertools.chain([first], costs):
+                values = [getattr(cost, field)
+                          for field in first._fields]
+                writer.writerow(values)
